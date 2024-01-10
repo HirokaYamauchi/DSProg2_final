959be2ad1fd2b09a9e4b80b72dcce9eb295f2fed
HirokaYamauchi <s2322067@stu.musashino-u.ac.jp>
2024-01-10 12:27:30 +0900

    スクレイピングしたデータを格納するデータベースを作成

--- DSProg2_final/DSProg2_final.py
+++ DSProg2_final/DSProg2_final.py
@@ -45,6 +45,28 @@ def scrape_weather_data(url):
 
     return data_list
 
+def create_database_table():
+    # SQLiteデータベース接続
+    conn = sqlite3.connect('weather_data.db')
+    cursor = conn.cursor()
+
+    # テーブルを作成するSQLクエリ
+    create_table_query = '''
+    CREATE TABLE IF NOT EXISTS weather (
+        id INTEGER PRIMARY KEY,
+        日付 TEXT NOT NULL,
+        降水量 TEXT,
+        平均気温 TEXT,
+        日照時間 TEXT,
+        天気概況_昼 TEXT
+    );
+    '''
+
+    # テーブルを作成
+    cursor.execute(create_table_query)
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
     url = "https://www.data.jma.go.jp/stats/etrn/view/daily_s1.php?prec_no=44&block_no=47662&year=2023&month=12&day=1&view="
     weather_data = scrape_weather_data(url)
