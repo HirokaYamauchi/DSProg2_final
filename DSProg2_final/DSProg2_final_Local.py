1eb8c69acbaa078a8b15613e5da099d012eb137a
HirokaYamauchi <s2322067@stu.musashino-u.ac.jp>
2024-01-12 21:17:26 +0900

    ローカルデータ用のinsert_into関数の追加

--- DSProg2_final/DSProg2_final_Local.py
+++ DSProg2_final/DSProg2_final_Local.py
@@ -21,6 +21,23 @@ def create_database_table2():
     conn2.commit()
     conn2.close()
 
+def insert_into_database(data_list2):
+    # SQLiteデータベース接続
+    conn2 = sqlite3.connect('weather_data.db')
+    cursor2 = conn2.cursor()
+
+    # データリストからデータベースにデータを入力
+    for data in data_list2:
+        cursor2.execute('''
+        INSERT INTO screentime (日付, 曜日, screen_time, よく使うアプリ)
+        VALUES (?, ?, ?, ?);
+        ''', (data['日付'], data['曜日'], data['screen_time'], data['よく使うアプリ']))
+
+    # 変更を保存して接続を閉じる
+    conn2.commit()
+    conn2.close()
+
+
 if __name__ == "__main__":
 
     # SQLiteデータベースとテーブルを作成
